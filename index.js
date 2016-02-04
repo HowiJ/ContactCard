6d05b7cb31dba0d19e538d528a68e5dc5fc7d2b7
Howard Jiang <howard.yunghao.jiang@gmail.com>
2016-02-04 15:28:22 -0800

    First working fully ( Can add new cards, can click to flip cards ). Affected: index.html, index.css, index.js

--- index.js
+++ index.js
@@ -1,6 +1,64 @@
-$( document ).ready( function () {
-	$( ".back" ).hide();
+function attachCardHand () {
 	$(".card").on("click", function() {
+		$(this).children().toggle();
 		console.log("click");
 	})	
+}
+
+$( document ).ready( function () {
+	var name = $("#firstName").val()+" "+$("#lastName").val();
+	var description = $("#description").val();
+	$( ".back" ).hide();
+	attachCardHand();
+
+	//Creates debugg cards
+	//createDebugCards();
+
+	$( document ).on("click", ".card",function() {
+		$(this).children().toggle();
+	})	
+
+	$("#submit").click( function() {
+		getValues();
+		makeCard();
+		return false;
+	})
+
+	//Flips the card on click *BROKEN*
+	function flip () {
+		$(this).children().hide();
+		console.log( $(this).attr("class")+$(this).attr("") );
+	}
+
+	//CARD CREATING FUNCTIONS//
+	//Creates a card in div
+	function makeCard() {
+		$("#cards").append( createCard(name, description) );
+		//$( ".back" ).hide();
+	}
+
+	//Gets values and places them in the correct variables
+	function getValues () {
+		name = $("#firstName").val()+" "+$("#lastName").val();
+		description = $("#description").val();
+	}
+
+	//Default cards pop up upon page load for debugging and testing.
+	// function createDebugCards () {
+	// 	name = "Tom Brady";
+	// 	description = "QB of the New England Patriots";
+	// 	makeCard();
+	// 	name = "Aaron Rodgers";
+	// 	description = "Best QB today"
+	// 	makeCard();
+	// 	name = "Peyton Manning"
+	// 	description = "QB guy"
+	// 	makeCard();
+	// }
 })
+
+//Creates a card using the inputted variables.
+function createCard (n, d) {
+	var app = "<div class='card'><div class='front'><h1>"+n+"</h1><p>Click for description</p></div><p class='back'>"+d+"</p></div>"
+	return app;
+}
